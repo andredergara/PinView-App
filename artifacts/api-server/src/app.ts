8362b5ab5d211c62c767eb7948bc8776e7fd9a97
andredergara <andredergarabusiness@gmail.com>
2026-05-20 13:41:22 -0400

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -15,17 +15,17 @@ app.set("trust proxy", 1);
 const PgStore = connectPgSimple(session);
 
 app.use(
-  pinoHttp({
+  pinoHttp.default({
     logger,
     serializers: {
-      req(req) {
+      req(req: any) {
         return {
           id: req.id,
           method: req.method,
           url: req.url?.split("?")[0],
         };
       },
-      res(res) {
+      res(res: any) {
         return {
           statusCode: res.statusCode,
         };
